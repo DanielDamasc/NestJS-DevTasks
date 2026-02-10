a6bb9e712fd14de3877db4742fd16e408d17fd81
DanielDamasc <dandamasceno04@gmail.com>
2026-02-10 18:22:46 -0300

    feat: Config CORS to permit requests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS para frontend fazer as requisições.
+  app.enableCors({
+    origin: 'http://localhost:5173', // Endereço do React
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
